0d37b0053722f1086a0b3d868bfe1f509f56bb87
fbanier <frbanier@gmail.com>
2025-06-23 15:29:16 +0200

    cinemateque

---
+++ 08-spring/exercice/rest/cinematheque/src/main/java/org/example/cinematheque/CinemathequeApplication.java
@@ -0,0 +1,13 @@
+package org.example.cinematheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemathequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemathequeApplication.class, args);
+    }
+
+}
