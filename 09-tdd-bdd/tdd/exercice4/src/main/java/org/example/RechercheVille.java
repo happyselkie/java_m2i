2a88eea1a6d3ec5b82d5c67a9febfaa97f52c9ba
fbanier <frbanier@gmail.com>
2025-06-30 11:52:21 +0200

    Tests recherche villes

---
+++ 09-tdd-bdd/tdd/exercice4/src/main/java/org/example/RechercheVille.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.example.exception.NotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RechercheVille {
+    private List<String> villes;
+
+    public RechercheVille() {
+        this.villes = List.of("Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul");
+    }
+
+    public List<String> rechercher(String mot)  {
+        if (mot.length() < 2){
+            if (mot == "*") return villes;
+            throw new NotFoundException("Cette ville n'existe pas");
+        }
+        if (mot.length() == 2) {
+            return villes.stream().filter(v -> v.toLowerCase().startsWith(mot.toLowerCase())).toList();
+        } else {
+            return villes.stream().filter(v -> v.toLowerCase().contains(mot.toLowerCase())).toList();
+        }
+    }
+
+
+
+
+
+    public List<String> getVilles() {
+        return villes;
+    }
+}
---
+++ 09-tdd-bdd/tdd/exercice4/src/test/java/RechercheVilleTest.java
@@ -0,0 +1,70 @@
+import org.example.RechercheVille;
+import org.example.exception.NotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class RechercheVilleTest {
+
+    private RechercheVille rechercheVille;
+    private List<String> villes;
+
+
+    @BeforeEach
+    public void setup(){
+        rechercheVille = new RechercheVille();
+        villes = rechercheVille.getVilles();
+    }
+
+
+    @Test
+    public void whenSearchWithV_ThenThrowException(){
+        String search = "V";
+
+        rechercheVille.rechercher(search);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {rechercheVille.rechercher(search);});
+    }
+
+    @Test
+    public void whenSearchWith_VA_ThenNamesStartingWith_VA(){
+        String search = "Va";
+
+        List<String> result = rechercheVille.rechercher(search);
+
+        Assertions.assertEquals(List.of("Valence", "Vancouver"), result);
+    }
+
+    @Test
+    public void whenSearchWithCaseSensitive_VA_ThenNamesStartingWith_VA(){
+        String search = "VA";
+
+        List<String> result = rechercheVille.rechercher(search);
+
+        Assertions.assertEquals(List.of("Valence", "Vancouver"), result);
+    }
+
+    @Test
+    public void whenSearchWith_ape_ThenBudapest(){
+        String search = "ape";
+
+        List<String> result = rechercheVille.rechercher(search);
+
+        Assertions.assertEquals(List.of("Budapest"), result);
+    }
+
+
+    @Test
+    public void whenSearchWithStar_ThenListOfVilles(){
+        String search = "*";
+
+        List<String> result = rechercheVille.rechercher(search);
+
+        Assertions.assertEquals(villes, result);
+    }
+
+
+
+}
