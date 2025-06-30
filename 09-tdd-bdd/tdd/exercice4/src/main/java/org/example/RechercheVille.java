cf2d5bdb7de80139a16f7e6c0936f4620bf12141
fbanier <frbanier@gmail.com>
2025-06-30 12:17:06 +0200

    Tests recherche villes

--- 09-tdd-bdd/tdd/exercice4/src/main/java/org/example/RechercheVille.java
+++ 09-tdd-bdd/tdd/exercice4/src/main/java/org/example/RechercheVille.java
@@ -3,7 +3,6 @@ package org.example;
 import org.example.exception.NotFoundException;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RechercheVille {
     private List<String> villes;
@@ -17,14 +16,6 @@ public class RechercheVille {
             if (mot.equals("*")) return villes;
             throw new NotFoundException("Cette ville n'existe pas");
         }
-        if (mot.length() == 2) {
-            return villes.stream().filter(v -> v.toLowerCase().startsWith(mot.toLowerCase())).toList();
-        } else {
-            return villes.stream().filter(v -> v.toLowerCase().contains(mot.toLowerCase())).toList();
-        }
-    }
-
-    public List<String> getVilles() {
-        return villes;
+        return villes.stream().filter(v -> v.toLowerCase().contains(mot.toLowerCase())).toList();
     }
 }
