fe1df8e97edc21f9a01b657f09f4bffbf31f482f
fbanier <frbanier@gmail.com>
2025-07-02 15:13:56 +0200

    exercice 1 microservice

---
+++ 10-microservice/exercice/exercice1/customer/src/main/java/org/example/customer/exception/NotFoundException.java
@@ -0,0 +1,4 @@
+package org.example.product.exception;
+
+public class NotFoundException extends RuntimeException {
+}
--- 10-microservice/exercice/exercice1/product/src/main/java/org/example/product/controller/ProductController.java
+++ 10-microservice/exercice/exercice1/product/src/main/java/org/example/product/controller/ProductController.java
@@ -1,19 +1,33 @@
 package org.example.product.controller;
 
+import org.example.product.dto.ProductReceiveDto;
+import org.example.product.dto.ProductResponseDto;
 import org.example.product.model.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.product.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("product")
 public class ProductController {
-    @GetMapping("/{id}")
-    public Product getProduct(@PathVariable int id) {
-        return new Product(id, "fromage",2.5 );
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> getAll() { return ResponseEntity.ok(productService.get()); }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> create(@RequestBody ProductReceiveDto product) { return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(product)); }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> getById(@PathVariable Integer id) { return ResponseEntity.ok(productService.get(id)); }
 }
 
 
