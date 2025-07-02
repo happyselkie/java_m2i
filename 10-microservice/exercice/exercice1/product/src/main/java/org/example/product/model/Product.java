bf5f3fdd2d9b94a17759f9486722b7924a380505
fbanier <frbanier@gmail.com>
2025-07-02 13:52:51 +0200

    exercice 1 microservice

--- 10-microservice/exercice/exercice1/product/src/main/java/org/example/product/model/Product.java
+++ 10-microservice/exercice/exercice1/product/src/main/java/org/example/product/model/Product.java
@@ -1,4 +1,16 @@
 package org.example.product.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
+    private int id;
+    private String name;;
+    private double price;
 }
